5923870664020e534bb638be21ab7c9cfbdffdf3
NEWisREAL <acc.newisreal@gmail.com>
2025-03-09 11:27:14 +0700

    📝util: add example config file

---
+++ example_config.php
@@ -0,0 +1,11 @@
+<!-- change the config and rename this file to `config.php` -->
+
+<?php
+$CONFIG = array(
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'your database username',
+    'DB_PASS' => 'your databaes password',
+    'DB_NAME' => 'your database name',
+    'DB_PORT' => '3306',
+    'ENCRYPTION_KEY' => '91e9b7f4-da09-4bdd-8d2f-63d89ecce9d2',
+);
